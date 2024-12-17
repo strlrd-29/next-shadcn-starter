d444c255ecc2dd366294ccb3219d3f87ddb2b2fc
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-12-17 07:55:57 +0100

    chore: moved globals.css to styles dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 
-import "./globals.css"
-
 import { cn } from "@/lib/utils"
+
+import "@/styles/globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
